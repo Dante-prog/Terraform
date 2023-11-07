4c6234ba65b6fecfc518addf93cde441faec21b4
Dante-prog <easton4984@gmail.com>
2023-11-07 15:35:45 -0500

    Updating repo

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ provider "linode" {
 resource "linode_instance" "web-instances" {
   count =  3
   label = "web-${count.index}"
-  image = var.image
+  image = var.linode_config.image
   private_ip = true
-  region = var.region
-  type = var.type_image
+  region = var.linode_config.region
+  type = var.linode_config.type_image
   root_pass = var.root_password
   authorized_keys = [var.ssh_public_key]
 }
--- variables.tf
+++ variables.tf
@@ -4,6 +4,23 @@ variable "linode_token" {
   
 }
 
+### OBJECT WAY OF CREATING VARIABLES
+variable "linode_config" {
+  description = "Configuration for the Linode instance"
+  type = object({
+    region         = string
+    image          = string
+    type_image     = string
+  })
+  default = {
+    region         = "us-east"
+    image          = "linode/ubuntu20.04"
+    type_image     = "g6-nanode-1"
+  }
+  // sensitive = true // Mark as sensitive if any of the attributes are sensitive.
+}
+
+### NORMAL WAY OF CREATING VARIABLES
 variable "root_password" {
     description = "The root password for the Linode instance"
     sensitive = true
@@ -13,19 +30,21 @@ variable "ssh_public_key" {
     description = "The public SSH key to be added to the Linode instance"
 }
 
-variable "region" {
-  type = string
-  default = "us-east"
-}
+# variable "region" {
+#   type = string
+#   default = "us-east"
+# }
 
-variable "image" {
-  type = string
-  default = "linode/ubuntu20.04"
+# variable "image" {
+#   type = string
+#   default = "linode/ubuntu20.04"
   
-}
+# }
 
-variable "type_image" {
-  type = string
-  default = "g6-nanode-1"
+# variable "type_image" {
+#   type = string
+#   default = "g6-nanode-1"
   
-}
+# }
+
+
